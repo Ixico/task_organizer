678198d22a9d36eeffadb5d2b60294dc251898d7
erykm <01159054@pw.edu.pl>
2022-06-26 01:03:54 +0200

    Initial commit

---
+++ src/test/java/com/ixico/application/TaskOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ixico.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskOrganizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
